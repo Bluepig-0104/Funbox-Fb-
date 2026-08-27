bda9bcdaa61ce88e47526bf6faffdff03334358f
Bluepig-0104 <dream90758@gmail.com>
2026-08-27 10:55:55 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,541 @@
+window.FUNBOX_STORES = [
+  {
+    "region": "北部",
+    "name": "Funbox 桃園遠東",
+    "lineId": "@bix0595a",
+    "brand": "Funbox",
+    "city": "桃園市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 桃園站前",
+    "lineId": "@fcm1241y",
+    "brand": "Funbox",
+    "city": "桃園市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 桃園環球A19",
+    "lineId": "@403qwxdn",
+    "brand": "Funbox",
+    "city": "桃園市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 中壢SOGO",
+    "lineId": "@xcs3672w",
+    "brand": "Funbox",
+    "city": "桃園市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 樹林秀泰",
+    "lineId": "@046txnme",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 板橋遠東",
+    "lineId": "@083zoknr",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 板橋大遠百",
+    "lineId": "@585clqqg",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 宏匯廣場",
+    "lineId": "@513vgbqo",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 中和環球",
+    "lineId": "@zgq9395a",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 比漾廣場",
+    "lineId": "@ufq5307a",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 新店裕隆城",
+    "lineId": "@491uicsm",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 淡水禮萊廣場",
+    "lineId": "@944creff",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 汐止遠雄",
+    "lineId": "@mhb3876c",
+    "brand": "Funbox",
+    "city": "新北市"
+  },
+  {
+    "region": "北部",
+    "name": "台北忠孝SOGO店",
+    "lineId": "@lcn7452p",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 三越南西",
+    "lineId": "@626iceit",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 天母SOGO",
+    "lineId": "@377cbfue",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 美麗華",
+    "lineId": "@660hextn",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "Funbox 南港 LaLaport",
+    "lineId": "@924ngwfb",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox 苗栗尚順",
+    "lineId": "@185vowmo",
+    "brand": "Funbox",
+    "city": "苗栗縣"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox 竹北遠東",
+    "lineId": "@642vdyvq",
+    "brand": "Funbox",
+    "city": "新竹縣"
+  },
+  {
+    "region": "中部",
+    "name": "新竹竹北想平方店",
+    "lineId": "@062qoqex",
+    "brand": "Funbox",
+    "city": "新竹縣"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox 新竹巨城",
+    "lineId": "@532otwmb",
+    "brand": "Funbox",
+    "city": "新竹市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox 新竹遠東",
+    "lineId": "@822rfnmr",
+    "brand": "Funbox",
+    "city": "新竹市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox 新竹遠雄",
+    "lineId": "@agl4214l",
+    "brand": "Funbox",
+    "city": "新竹市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox Toys- 豐原太平洋",
+    "lineId": "@094mvrhb",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "FunBox Toys 台中漢神洲際店",
+    "lineId": "@218xxrbx",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox 台中中友店",
+    "lineId": "@815dstuy",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox Toys- 廣三SOGO",
+    "lineId": "@526bsjmb",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox Toys- 台中三越",
+    "lineId": "@833rhkmr",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox Toys- 台中遠東",
+    "lineId": "@147vfxjr",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox Toys- 台中新時代",
+    "lineId": "@hdg3289a",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox Toys- 台中Lalaport",
+    "lineId": "@620dfrfm",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox Toys- 文心秀泰",
+    "lineId": "@605dilqq",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "Funbox Toys- 台中港三井",
+    "lineId": "@816xpruh",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "中部",
+    "name": "來玩聚-彰化家樂福",
+    "lineId": "@766sgllj",
+    "brand": "來玩聚",
+    "city": "彰化縣"
+  },
+  {
+    "region": "中部",
+    "name": "來玩聚-員林大潤發",
+    "lineId": "@958tzuco",
+    "brand": "來玩聚",
+    "city": "彰化縣"
+  },
+  {
+    "region": "中部",
+    "name": "來玩聚-斗六家樂福",
+    "lineId": "@rsp8657s",
+    "brand": "來玩聚",
+    "city": "雲林縣"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox Toys- 台南三井",
+    "lineId": "@212isujx",
+    "brand": "Funbox",
+    "city": "台南市"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox Toys- 高雄漢神",
+    "lineId": "@897upgfr",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox Toys- 漢神巨蛋",
+    "lineId": "@ldh6278d",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox Toys- 高雄左營",
+    "lineId": "@obz8096l",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox Toys- 夢時代 (二館)",
+    "lineId": "@ott3541o",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox Toys- 高雄大立",
+    "lineId": "@917hatdy",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox Toys- 義享天地",
+    "lineId": "@777nkbeo",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox 義大2館",
+    "lineId": "@bxd6822t",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "來玩聚-鳳山店",
+    "lineId": "@189yvcce",
+    "brand": "來玩聚",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "來玩聚-新楠店",
+    "lineId": "@022itqlw",
+    "brand": "來玩聚",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "來玩聚-楠梓店",
+    "lineId": "@241cmnww",
+    "brand": "來玩聚",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox Toys- 屏東環球",
+    "lineId": "@xhi2668e",
+    "brand": "Funbox",
+    "city": "屏東縣"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox 屏東太平洋",
+    "lineId": "@ess7711t",
+    "brand": "Funbox",
+    "city": "屏東縣"
+  },
+  {
+    "region": "南部",
+    "name": "來玩聚-新屏店",
+    "lineId": "@308oaews",
+    "brand": "來玩聚",
+    "city": "屏東縣"
+  },
+  {
+    "region": "南部",
+    "name": "Funbox 高雄大遠百",
+    "lineId": "@204edsnm",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "高雄SOGO店",
+    "lineId": "@845nallq",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "南部",
+    "name": "台南南紡購物中心",
+    "lineId": "@msq0107x",
+    "brand": "Funbox",
+    "city": "台南市"
+  },
+  {
+    "region": "南部",
+    "name": "台南遠百店",
+    "lineId": "@373rxyat",
+    "brand": "Funbox",
+    "city": "台南市"
+  },
+  {
+    "region": "南部",
+    "name": "台南新天地",
+    "lineId": "@344rybaf",
+    "brand": "Funbox",
+    "city": "台南市"
+  },
+  {
+    "region": "南部",
+    "name": "嘉義耐斯",
+    "lineId": "@121vsdww",
+    "brand": "Funbox",
+    "city": "嘉義市"
+  },
+  {
+    "region": "南部",
+    "name": "嘉義三越",
+    "lineId": "@zhj4962p",
+    "brand": "Funbox",
+    "city": "嘉義市"
+  },
+  {
+    "region": "東部",
+    "name": "Funbox 宜蘭新月",
+    "lineId": "@027iendl",
+    "brand": "Funbox",
+    "city": "宜蘭縣"
+  },
+  {
+    "region": "東部",
+    "name": "Funbox 花蓮店",
+    "lineId": "@ymc0892z",
+    "brand": "Funbox",
+    "city": "花蓮縣"
+  },
+  {
+    "region": "北部",
+    "name": "遠百信義 A13",
+    "lineId": "@020sqlyp",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "中壢大江",
+    "lineId": "@099ldnvw",
+    "brand": "Funbox",
+    "city": "桃園市"
+  },
+  {
+    "region": "北部",
+    "name": "天母三越",
+    "lineId": "@237annfd",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "台茂購物中心",
+    "lineId": "@504tdsbb",
+    "brand": "Funbox",
+    "city": "桃園市"
+  },
+  {
+    "region": "北部",
+    "name": "三越站前",
+    "lineId": "@631ovgxp",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "潤泰南港車站店",
+    "lineId": "@924tguor",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "信義新天地 A8 館",
+    "lineId": "@983dfazy",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "北部",
+    "name": "桃園環球 A8",
+    "lineId": "@lae4656h",
+    "brand": "Funbox",
+    "city": "桃園市"
+  },
+  {
+    "region": "北部",
+    "name": "高島屋百貨",
+    "lineId": "@xbm4599s",
+    "brand": "Funbox",
+    "city": "台北市"
+  },
+  {
+    "region": "中部",
+    "name": "麗寶一期",
+    "lineId": "@829adhop",
+    "brand": "Funbox",
+    "city": "台中市"
+  },
+  {
+    "region": "南部",
+    "name": "嘉義遠東",
+    "lineId": "@gno1826d",
+    "brand": "Funbox",
+    "city": "嘉義市"
+  },
+  {
+    "region": "離島",
+    "name": "澎坊商場",
+    "lineId": "@185ybuaz",
+    "brand": "Funbox",
+    "city": "澎湖縣"
+  },
+  {
+    "region": "南部",
+    "name": "大魯閣新光",
+    "lineId": "@fko7562c",
+    "brand": "Funbox",
+    "city": "高雄市"
+  },
+  {
+    "region": "東部",
+    "name": "台東秀泰",
+    "lineId": "@438glrqv",
+    "brand": "Funbox",
+    "city": "台東縣"
+  },
+  {
+    "region": "南部",
+    "name": "來玩聚-台南新仁家樂福",
+    "lineId": "@162fxyom",
+    "brand": "來玩聚",
+    "city": "台南市"
+  },
+  {
+    "region": "東部",
+    "name": "來玩聚-台東家樂福",
+    "lineId": "@114pqrny",
+    "brand": "來玩聚",
+    "city": "台東縣"
+  },
+  {
+    "region": "北部",
+    "name": "來玩聚-台北地下街16號",
+    "lineId": "@453cqkyj",
+    "brand": "來玩聚",
+    "city": "台北市"
+  }
+];
